781c03f6c6827fc8fd032359e95762edc421a15b
bthalheim <ben@bthal.com>
2024-12-06 21:28:32 -0500

    C# Day 1 Added

--- cs/day1/part2.cs
+++ cs/day1/part2.cs
@@ -10,31 +10,47 @@ class Program
         List<int> right = new List<int>();
         List<int> left = new List<int>();
 
+        Dictionary<int, int> table = new Dictionary<int, int>();
+
 
         string line;
         while ((line = Console.ReadLine()) != null && line != "") 
         {
             string[] parts = line.Split(new string[] {"  "}, StringSplitOptions.None);
             
-            right.Add(int.Parse(parts[1]));
             left.Add(int.Parse(parts[0]));
-
+            right.Add(int.Parse(parts[1]));
         }
-
-        right.Sort();
-        left.Sort();
 
         int count = 0;
 
+        for (int i = 0; i < left.Count; i++)
+        {
+            if (!table.ContainsKey(left[i]))
+            {
+                table[left[i]] = 0;
+            } 
+
+        }
+        
         for (int i = 0; i < right.Count; i++)
         {
-            count += Math.Abs(right[i]-left[i]); 
+            if (table.ContainsKey(right[i]))
+            {
+                table[right[i]]++;
+            }
         }
+
+
+        for (int i = 0; i < left.Count; i++)
+        {
+            count += left[i] * table[left[i]];
+        }
+
 
         Console.WriteLine(count);
 
     }
-
 
 }
 
